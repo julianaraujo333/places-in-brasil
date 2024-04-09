079d89374a06e0e481d4dc8076ecb5f015929bfe
Juliana <julianaraujo555@outlook.com>
2024-04-09 18:56:55 -0300

    adding nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,21 @@
-const iconMenu = document.querySelector(".nav--icon")
-const navMenu = document.querySelector(".nav--menu")
-
-
-console.log('OK')
-
-iconMenu.addEventListener('click', function(){
-	if (navMenu.style.display === 'none' || navMenu.style.display === '') {
-		navMenu.style.display = 'block';
-		navMenu.style.height = 'auto';
-		navMenu.style.opacity = '1';
-		iconMenu.setAttribute('aria-expanded', 'true');
-		iconMenu.setAttribute('aria-hidden', 'false');
-	} else {
-		navMenu.style.display = 'none';
-		navMenu.style.height = '0';
-		navMenu.style.opacity = '0';
-		iconMenu.setAttribute('aria-expanded', 'false');
-		iconMenu.setAttribute('aria-hidden', 'true');
-	}
-});
-
-
-function toggleMenu() {
-	if (window.innerWidth > 790) {
-		if (navMenu.style.display === 'none' || navMenu.style.display === '') {
-			navMenu.style.display = 'flex';
-			navMenu.style.height = 'auto';
-			navMenu.style.opacity = '1';
-			iconMenu.setAttribute('aria-expanded', 'true');
-      iconMenu.setAttribute('aria-hidden', 'false');
-		} else {
-			navMenu.style.display = 'none';
-			navMenu.style.height = '0';
-			navMenu.style.opacity = '0';
-			iconMenu.setAttribute('aria-expanded', 'false');
-			iconMenu.setAttribute('aria-hidden', 'true');
-		}
-	} else {
-		navMenu.style.display = 'none';
-		navMenu.style.height = '0';
-		navMenu.style.opacity = '0';
-		iconMenu.setAttribute('aria-expanded', 'false');
-		iconMenu.setAttribute('aria-hidden', 'true');
-	}
+function initialize() {
+  const fenway = { lat: 42.345573, lng: -71.098326 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: fenway,
+    zoom: 14,
+  });
+  const panorama = new google.maps.StreetViewPanorama(
+    document.getElementById("pano"),
+    {
+      position: fenway,
+      pov: {
+        heading: 34,
+        pitch: 10,
+      },
+    },
+  );
+		console.log('entrou')
+  map.setStreetView(panorama);
 }
 
-toggleMenu();
-
-window.addEventListener('resize', toggleMenu);
+initialize()
